f48a3044f0084d54200ff968e35b468814279844
Prafful Agarwal <prafful.agrwal@gmail.com>
2017-10-21 20:49:12 +0530

    Update test.c

--- Memory Leak Detection/test.c
+++ Memory Leak Detection/test.c
@@ -2,7 +2,10 @@
 #include	"leak_detector_c.h"
 int main()
 {
+	// "malloc" is mapped to custom method "xmalloc" defined in "leak_detector_c.h"
 	char * ptr1 = (char *)malloc(10);
+	
+	// "calloc" is mapped to custom method "xcalloc" defined in "leak_detector_c.h"
 	int * ptr2 = (int *)calloc(10, sizeof(int));
 	float * ptr3 = (float *) calloc(15, sizeof(float));
 	
