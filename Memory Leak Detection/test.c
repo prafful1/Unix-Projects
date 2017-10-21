5b7895aa9663483881dd74a1de6b876a731e819e
Prafful Agarwal <prafful.agrwal@gmail.com>
2017-10-21 20:47:03 +0530

    Update test.c

--- Memory Leak Detection/test.c
+++ Memory Leak Detection/test.c
@@ -5,7 +5,11 @@ int main()
 	char * ptr1 = (char *)malloc(10);
 	int * ptr2 = (int *)calloc(10, sizeof(int));
 	float * ptr3 = (float *) calloc(15, sizeof(float));
+	
+	// at this point, only the memory in heap pointed by ptr2 is freed.
 	free(ptr2);
+	
+	// "atexit" metohd registetrs "report_mem_leak" method to be called during exit process by "startup routine".
 	atexit(report_mem_leak);
 	return 0;
 }
